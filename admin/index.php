338668b57a4788d44c247a456c0913fa1c068f9b
MD FIROJ UDDIN <48468312+FirojUddinA@users.noreply.github.com>
2022-04-21 15:11:39 +0600

    aaa
    aaaa

--- admin/index.php
+++ admin/index.php
@@ -1,28 +1,14 @@
-<?php
-session_start();
-$role = 0;
-if (isset($_SESSION['role'])){
-    $role =($_SESSION['role']);
-}
-if(isset($_SESSION['user']) && isset($_SESSION['role']) && $role === 11202){
+<?php include "templates/admin-header.php"; ?>
 
-}
-else{
-    header("location:login.php");
-}
-?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Admin dashboard</title>
 </head>
 <body>
-<h1>Admin</h1>
+
+<div class="wrapper">
+
+<?php include "templates/admin-navigation.php"; ?>
 <?php echo '<a href="../logout.php">Logout</a>';?>
-</body>
-</html>
+
+
+<?php include "templates/admin-footer.php"; ?>
---
+++ admin/templates/admin-header.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+$role = 0;
+if (isset($_SESSION['role'])){
+    $role =($_SESSION['role']);
+}
+if(isset($_SESSION['user']) && isset($_SESSION['role']) && $role === 11202){
+
+}
+
+else{
+    header("location:login.php");
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="../assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="../assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="../assets/plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="../assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="../assets/plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="../assets/plugins/summernote/summernote-bs4.min.css">
