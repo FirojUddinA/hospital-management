1d464c6bda28196cac7e7065516503b81dae9c0e
MD FIROJ UDDIN <48468312+FirojUddinA@users.noreply.github.com>
2022-04-21 23:23:03 +0600

    44

---
+++ doctors/templates/doctor-navigation.php
@@ -0,0 +1,173 @@
+<?php
+
+$user_id = 0;
+if (isset($_SESSION['user'])){
+    $user_id =($_SESSION['user']);
+
+}
+include '../config.php';
+
+$sql = "SELECT * FROM `details` WHERE  user_id='1'";
+
+//        seve to db and check
+$result  = mysqli_query($conn, $sql);
+$user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+//print_r($user);
+?>
+
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="index3.html" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="#" class="nav-link">Contact</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="../logout.php" class="nav-link">Logout</a>
+        </li>
+    </ul>
+
+    <!-- Right navbar links -->
+</nav>
+<!-- /.navbar -->
+
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+        <img src="../assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">H M S</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="../assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block text-uppercase"><?php echo $user['f_name'] .' '. $user['l_name'] ?></a>
+            </div>
+        </div>
+
+        <!-- SidebarSearch Form
+        <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+         Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas  fa-hospital-user"></i>
+                        <p>
+                            Apoinment
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="doctor-new.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Add New Doctor</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="doctors-all.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>All doctors</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-user-cog"></i>
+                        <p>
+                            Admin
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="admin-new.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Add New Admin</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="admin-all.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>All Admins</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-user-cog"></i>
+                        <p>
+                            User
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="new-doctor.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Add New Doctor</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="all-doctors.php" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>All doctors</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="profile.php" class="nav-link">
+                        <i class="nav-icon far fa-calendar-alt"></i>
+                        <p>
+                            Profile
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="../logout.php" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out text-info"></i>
+                        <p>Logout</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
